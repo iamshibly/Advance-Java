2db475c568700e927c10fe91bc637ed41a323c98
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-08-27 21:47:38 +0600

    Add files via upload

---
+++ 2. PrintPattern/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Right Half Pyramid: \n");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *\n");
+
+        System.out.print("Reverse Right Half Pyramid: \n");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*\n");
+
+        System.out.print("Left Half Pyramid: \n");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+
+    }
+}
