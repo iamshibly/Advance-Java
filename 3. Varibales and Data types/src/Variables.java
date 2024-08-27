6fc84cba36cefe99f58e15d0a7e9cdf62316ceb8
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-08-27 23:55:31 +0600

    Add files via upload

---
+++ 3. Varibales and Data types/src/Variables.java
@@ -0,0 +1,31 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        System.out.println(myNumber);
+        int yourNumber = 0;
+        System.out.println(yourNumber);
+        yourNumber = 865;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+        System.out.println(yourNumber);
+
+        float myFloat = 5.0f;
+        double myDouble = 6.98766;
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+
+        char myCharacter = 'K';
+        System.out.println(myCharacter);
+
+        System.out.println("Josss");
+
+
+
+    }
+}
