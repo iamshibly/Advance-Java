96aedac44d0add0e4dd0c50ef564704825a4ab83
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-08-27 17:34:36 +0600

    Add files via upload

---
+++ 1. MyFirstCode/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String [] args) {
+        System.out.print("Welcome");
+        System.out.println("KG");
+        System.out.println("Coding");
+        System.out.print("josss");
+
+    }
+}
