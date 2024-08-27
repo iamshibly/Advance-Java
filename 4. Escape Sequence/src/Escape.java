5b5756943a944a23bc2e7ddffb43cbeb9107c5b5
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-08-28 00:18:06 +0600

    Add files via upload

---
+++ 4. Escape Sequence/src/Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("Hello \\KGCoding\\...");
+        System.out.println("Hello \"KGCoding\"...");
+        System.out.println("Hello 'KGCoding'...");
+        System.out.println("Hello \bKGCoding...");
+        System.out.println("Hello \tKGCoding\\...");
+        System.out.println("Hello \nKGCoding...");
+        //royalmadrid
+    }
+}
