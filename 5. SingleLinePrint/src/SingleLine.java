0c24d3d1ebef116792b4fb3438b614f944b83433
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-08-28 00:30:01 +0600

    Add files via upload

---
+++ 5. SingleLinePrint/src/SingleLine.java
@@ -0,0 +1,7 @@
+public class SingleLine {
+    public static void main(String[] args) {
+        System.out.println("*\n*\t*\n*\t*\t*\n*\t*\t*\t*\n*\t*\t*\t*\t*\n");//Right Half Pyramid
+        System.out.println("*\t*\t*\t*\t*\n*\t*\t*\t*\n*\t*\t*\n*\t*\n*\n");//Reverse Right Half Pyramid
+        System.out.println("\t\t\t\t*\n\t\t\t*\t*\n\t\t*\t*\t*\n\t*\t*\t*\t*\n*\t*\t*\t*\t*\n");//Left Half Pyramid
+    }
+}
