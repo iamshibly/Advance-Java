d34c99d4b609d05f59cf95ab65448f2e41db81b2
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-08-28 17:04:14 +0600

    Add files via upload

---
+++ 12. Area of a Triangle/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Base of a triangle in cm: ");
+        double Base = input.nextDouble();
+        System.out.print("Enter the Height of a triangle in cm ");
+        double Height = input.nextDouble();
+
+        System.out.println("Area of a Triangle: " + (0.5*Base*Height) + "cm");
+
+    }
+}
