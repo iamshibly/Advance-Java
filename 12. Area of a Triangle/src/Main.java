29985662281e539cf214faed4b5e67aca06ca56a
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2024-08-28 17:10:06 +0600

    Update Main.java

--- 12. Area of a Triangle/src/Main.java
+++ 12. Area of a Triangle/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.print("Enter the Height of a triangle in cm ");
         double Height = input.nextDouble();
 
-        System.out.println("Area of a Triangle: " + (0.5*Base*Height) + "cm");
+        System.out.println("Area of a Triangle: " + (0.5*Base*Height) + "cm^2");
 
     }
 }
